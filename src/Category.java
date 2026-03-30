3acd7db7665d5e0c3bd0ae924cdc917f726669ea
norder112 <derild112@gmail.com>
2026-03-30 17:28:41 +0300

    аБСТРАКТНЫЙ КЛАСС

--- src/Category.java
+++ src/Category.java
@@ -5,4 +5,12 @@ public abstract class Category extends Product{
     private HashMap<Integer, String> category;
     private Category listCategory;
     Category(){}
+
+    public abstract void addCategory(Integer id, String title){
+        this.category.put(id,title);
+    }
+
+    public void showCategory(){
+        System.out.println(this.category);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,11 @@ public class Main {
     public static void main(String[] args) {
         HashMap<Integer, String> l = new HashMap<Integer, String>();
         l.put(42,"dsf");
-        Product product1 = new Elcetronic("Комп",42,l);
-        Product product2 = new Elcetronic("телефон",42,l);
-        Product product3 = new Elcetronic("Эпштейн",142,l);
+        // String title, double price, HashMap<Integer, String> category
+        Product product42 = new Category("Комп", 42, l) ;
 
-        System.out.println(product1);
-        System.out.println(product2);
-        System.out.println(product3);
-
+        product42.addCategory(1,"Зубачситки");
+        product42.addCategory(2,"машины");
+        product42.showCategory();
     }
 }
